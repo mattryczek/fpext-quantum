d58de4dfcb43ef675675ef67b2e59e42919e7940
Mateusz Ryczek <mateusz@u.northwestern.edu>
2021-04-27 12:35:13 -0500

    Fix home button

--- js/cards.js
+++ js/cards.js
@@ -3,16 +3,16 @@ handle_errors();
 let tickets = document.getElementsByClassName("x-grid3-row");
 transform();
 
-async function clean_up(){
+async function clean_up() {
     //Disable FP auto refresh so we can inject our own
     //quickEditUpdateInProgress = true;
 
     let trash = document.body.children;
 
-    while(trash.length > 1){
-        if(trash[0].id !== "ContentWrapper"){
+    while (trash.length > 1) {
+        if (trash[0].id !== "ContentWrapper") {
             trash[0].remove();
-        }else{
+        } else {
             trash[1].remove();
         }
     }
@@ -26,30 +26,28 @@ async function clean_up(){
 
 }
 
-function handle_errors(){
-  let maybe_error;
+function handle_errors() {
+    let maybe_error;
 
-  try{
-    maybe_error = document.getElementsByClassName("pageHeader")[0].textContent;
+    try {
+        maybe_error = document.getElementsByClassName("pageHeader")[0].textContent;
 
-    switch (maybe_error) {
-      case "Login Error":
-        create_relog();
-        break;
+        switch (maybe_error) {
+            case "Login Error":
+                create_relog();
+                break;
 
-      case "FootPrints Service Core Error":
-        let msg = document.getElementsByClassName("dialogMainContent")[0].innerText;
-        create_error(msg);
-        break;
-    }
-  }
-  catch{
-  }
+            case "FootPrints Service Core Error":
+                let msg = document.getElementsByClassName("dialogMainContent")[0].innerText;
+                create_error(msg);
+                break;
+        }
+    } catch {}
 }
 
-function transform(){
+function transform() {
 
-    if(document.getElementsByClassName("x-grid-empty").length === 1){
+    if (document.getElementsByClassName("x-grid-empty").length === 1) {
         no_tickets();
     } else {
         document.body.prepend(create_container(tickets.length, 3));
@@ -63,12 +61,12 @@ function transform(){
 
 }
 
-function no_tickets(){
+function no_tickets() {
     let parent = document.getElementById("grid-ct").parentNode;
 
     let banner;
 
-    switch (document.getElementsByClassName("x-grid-empty")[0].textContent){
+    switch (document.getElementsByClassName("x-grid-empty")[0].textContent) {
         case "There were no matches to your query.":
             banner = create_banner("There were no matches to your query", "alert-danger");
             break;
@@ -81,32 +79,32 @@ function no_tickets(){
     parent.appendChild(banner);
 }
 
-function highlight(card){
+function highlight(card) {
     card.classList.remove("shadow-sm");
     card.classList.add("shadow");
 }
 
-function lowlight(card){
+function lowlight(card) {
     card.classList.remove("shadow");
     card.classList.add("shadow-sm");
 }
 
-function signout(){
-    location.href="/MRcgi/MRlogout.pl?USER=" + document.quickSearch.USER.value + "&MRP="+ document.quickSearch.MRP.value + "&PROJECTID=" + document.quickSearch.PROJECTID.value + "&LASTID=" + document.quickSearch.LASTID.value;
+function signout() {
+    location.href = "/MRcgi/MRlogout.pl?USER=" + document.quickSearch.USER.value + "&MRP=" + document.quickSearch.MRP.value + "&PROJECTID=" + document.quickSearch.PROJECTID.value + "&LASTID=" + document.quickSearch.LASTID.value;
 }
 
-function prefs(){
-    location.href="/MRcgi/MRpreferences_page.pl?USER=" + document.quickSearch.USER.value + "&MRP="+ document.quickSearch.MRP.value + "&PROJECTID=" + document.quickSearch.PROJECTID.value + "&LASTID=" + document.quickSearch.LASTID.value;
+function prefs() {
+    location.href = "/MRcgi/MRpreferences_page.pl?USER=" + document.quickSearch.USER.value + "&MRP=" + document.quickSearch.MRP.value + "&PROJECTID=" + document.quickSearch.PROJECTID.value + "&LASTID=" + document.quickSearch.LASTID.value;
 }
 
-function init_navbar(){
-    document.getElementById("go_home").href = '/MRcgi/MRhomepage.pl?USER="' + document.quickSearch.USER.value + "&MRP="+ document.quickSearch.MRP.value + "&PROJECTID=" + document.quickSearch.PROJECTID.value + "&LASTID=" + document.quickSearch.LASTID.value;
+function init_navbar() {
+    document.getElementById("go_home").href = '/MRcgi/MRhomepage.pl?USER=' + document.quickSearch.USER.value + "&MRP=" + document.quickSearch.MRP.value + "&PROJECTID=" + document.quickSearch.PROJECTID.value + "&LASTID=" + document.quickSearch.LASTID.value;
 
     let reports = document.getElementById("tb_kb_report").parentElement.parentElement.children;
     let report_menu = document.getElementById("reports_drop");
     let report;
 
-    while(reports.length !== 0){
+    while (reports.length !== 0) {
         report = reports[0].firstChild
         report.classList = "dropdown-item";
 
@@ -130,7 +128,7 @@ function init_navbar(){
     const searchby = document.quickSearch.WSEARCH.value;
     const indicate = document.getElementById("query_drop");
 
-    if (searchby === "KEYWORD"){
+    if (searchby === "KEYWORD") {
         indicate.children[1].classList.add("active");
         indicate.children[0].classList.remove("active");
     } else {
@@ -143,10 +141,15 @@ function init_navbar(){
     let username = document.getElementsByClassName("breadcrumbdarkblue")[0].textContent.split(' ')[1];
     document.getElementById("username").textContent = "Welcome, " + username + "!";
 
-    document.getElementById("search_input").onkeypress = function(e) { if(e.keyCode === 13) { do_search(); return false;}}
+    document.getElementById("search_input").onkeypress = function(e) {
+        if (e.keyCode === 13) {
+            do_search();
+            return false;
+        }
+    }
 }
 
-async function get_full_desc(button, ticket_num){
+async function get_full_desc(button, ticket_num) {
     let loading = document.createElement('span');
     loading.classList = "spinner-border spinner-border-sm ml-2";
 
@@ -156,9 +159,10 @@ async function get_full_desc(button, ticket_num){
     let usr = document.quickSearch.USER.value;
 
     Ext.get('desc-body')
-        .load({url: '/MRcgi/MRAjaxShowDescriptions.pl?USER=' + usr + '&MRP=' + mrp + '&MR=' + ticket_num + '&PROJECTID=1',
+        .load({
+            url: '/MRcgi/MRAjaxShowDescriptions.pl?USER=' + usr + '&MRP=' + mrp + '&MR=' + ticket_num + '&PROJECTID=1',
             callback: function(el, success) {
-                if(success){
+                if (success) {
                     let desc = document.getElementById("desc-body").firstElementChild;
                     document.getElementById("descm_title").textContent = desc.firstElementChild.textContent;
                     desc.firstElementChild.remove();
@@ -175,76 +179,73 @@ async function get_full_desc(button, ticket_num){
         });
 }
 
-function get_ticket_num(ticket){
+function get_ticket_num(ticket) {
     return ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_mr")[0].firstElementChild.firstElementChild.textContent;
 }
 
-function get_title(ticket){
+function get_title(ticket) {
     return ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_title")[0].firstElementChild.firstElementChild.textContent;
 }
 
-function get_description(ticket){
+function get_description(ticket) {
     let desc = ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_title")[0].firstChild.childNodes[1].textContent;
 
-    if(desc.length === 1){
-      desc = 'Enable "Collapsed Description in Title" within your prefs to show previews!';
+    if (desc.length === 1) {
+        desc = 'Enable "Collapsed Description in Title" within your prefs to show previews!';
     }
 
-    if(typeof desc === undefined){
-      desc = 'Enable "Collapsed Description in Title" within your prefs to show previews!';
+    if (typeof desc === undefined) {
+        desc = 'Enable "Collapsed Description in Title" within your prefs to show previews!';
     }
 
     return desc;
 }
 
-function get_status(ticket){
-  let status;
+function get_status(ticket) {
+    let status;
 
-  try{
-    status = ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_status")[0].firstChild.textContent;
+    try {
+        status = ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_status")[0].firstChild.textContent;
 
-  }
-  catch{
-    status = "DISABLED";
-  }
+    } catch {
+        status = "DISABLED";
+    }
     return status;
 }
 
-function get_last_edit(ticket){
+function get_last_edit(ticket) {
     return ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_datetimeago")[0].firstChild.textContent;
 }
 
-function get_priority(ticket){
-  let priority;
+function get_priority(ticket) {
+    let priority;
 
-  try {
-    priority = ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_priority")[0].firstChild.textContent;
-  }
-  catch{
-    priority = "DISABLED";
-  }
-
-  return priority;
-  }
-
-function dept_support(ticket){
-  let support;
-
-  try{
-    support = ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_Departmental__bSupport")[0].firstChild.textContent;
-
-    if(support === '-'){
-        support = "none";
+    try {
+        priority = ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_priority")[0].firstChild.textContent;
+    } catch {
+        priority = "DISABLED";
     }
-  }
-  catch{
-    support = "DISABLED";
-  }
+
+    return priority;
+}
+
+function dept_support(ticket) {
+    let support;
+
+    try {
+        support = ticket.rows[0].getElementsByClassName("x-grid3-td-hpColHeading_Departmental__bSupport")[0].firstChild.textContent;
+
+        if (support === '-') {
+            support = "none";
+        }
+    } catch {
+        support = "DISABLED";
+    }
 
     return support;
 }
 
-function badge_type(ticket){
+function badge_type(ticket) {
     const badges = {
         LOW: 'badge-success',
         MEDIUM: 'badge-warning',
@@ -257,7 +258,7 @@ function badge_type(ticket){
     let priority = get_priority(ticket);
     let badge = badges.LOW;
 
-    switch(priority){
+    switch (priority) {
         case "SR-MED":
         case "I-MEDIUM":
             badge = badges.MEDIUM;
@@ -303,15 +304,15 @@ async function restore_badge(badge, ticket_num) {
     badge.removeAttribute("onmouseleave");
 }
 
-function create_relog(){
-  document.getElementById("ContentWrapper").setAttribute('style', 'display:block !important; top: 0px !important; left: 0px !important; margin: 0 auto; position: initial !important');
+function create_relog() {
+    document.getElementById("ContentWrapper").setAttribute('style', 'display:block !important; top: 0px !important; left: 0px !important; margin: 0 auto; position: initial !important');
 }
 
-function create_error(msg){
-  document.getElementById("ContentWrapper").setAttribute('style', 'display:block !important; top: 0px !important; left: 0px !important; margin: 0 auto; position: initial !important');
+function create_error(msg) {
+    document.getElementById("ContentWrapper").setAttribute('style', 'display:block !important; top: 0px !important; left: 0px !important; margin: 0 auto; position: initial !important');
 }
 
-function create_banner(text, type){
+function create_banner(text, type) {
     let banner = document.createElement('div');
     banner.classList = "m-4 alert " + type;
     banner.textContent = text;
@@ -319,24 +320,24 @@ function create_banner(text, type){
     return banner;
 }
 
-function create_row(){
+function create_row() {
     let row = document.createElement('div');
     row.classList = "card-deck mb-3";
 
     return row;
 }
 
-function create_card(){
+function create_card() {
     let curr_ticket = tickets[0].firstChild;
     let ticket_num = get_ticket_num(curr_ticket);
 
     let card = document.createElement('div');
     card.classList = "card shadow-sm";
-    if(get_priority(curr_ticket) === "I-CRITICAL"){
+    if (get_priority(curr_ticket) === "I-CRITICAL") {
         card.classList.add("border-danger")
     }
     card.setAttribute("onmouseover", "highlight(this)");
-    card.setAttribute("onmouseout" , "lowlight(this)");
+    card.setAttribute("onmouseout", "lowlight(this)");
 
     let card_body = document.createElement('div');
     card_body.classList = "card-body";
@@ -356,7 +357,7 @@ function create_card(){
 
     let dept_badge = document.createElement('span');
     let support = dept_support(curr_ticket);
-    if(support !== "none"){
+    if (support !== "none") {
         dept_badge.textContent = support;
         dept_badge.classList = "badge badge-info float-left mr-1 my-1";
     }
@@ -373,7 +374,7 @@ function create_card(){
     let modal_button = document.createElement('button');
     modal_button.classList = "btn btn-sm btn-outline-secondary mr-2";
     modal_button.textContent = "Full Description";
-    modal_button.setAttribute("onclick", "get_full_desc(this, "+ ticket_num + ")");
+    modal_button.setAttribute("onclick", "get_full_desc(this, " + ticket_num + ")");
 
     let details_button = document.createElement('button');
     details_button.classList = "btn btn-sm btn-outline-secondary mr-2";
@@ -417,7 +418,7 @@ function create_card(){
     return card;
 }
 
-function create_modal(){
+function create_modal() {
     let modal_shim = document.createElement('div');
 
     modal_shim.innerHTML = '<div class="modal fade" id="desc_modal" tabindex="-1" role="dialog"> <div class="modal-dialog modal-dialog-centered modal-lg"> <div class="modal-content"> <div class="modal-header"> <h6 class="modal-title" id="descm_title">Modal title</h6> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div> <div class="modal-body"> <div class="d-flex justify-content-center" id="descm_body"> <div class="spinner-border" role="status"> <span class="sr-only">Loading...</span> </div> </div> </div> <div class="modal-footer"> <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary" id="descm_edit">Edit</button> </div> </div> </div> </div>';
@@ -427,7 +428,7 @@ function create_modal(){
     document.body.appendChild(modal);
 }
 
-function create_navbar(){
+function create_navbar() {
     let navbar_shim = document.createElement('div');
 
     navbar_shim.innerHTML = '<div class="fixed-top" id="navwhole"> <div class="collapse" id="hidden_opts"> <div class="bg-light p-4"> <h4 id="username" class="mb-3">Welcome, $User!</h4> <button type="button" class="btn btn-danger" onclick="signout()">Sign Out</button> <button type="button" class="btn btn-outline-secondary" onclick="prefs()">Preferences</button> </div> </div> <nav class="border-top border-bottom border-secondary navbar navbar-expand-md navbar-light bg-light" id="justbar"> <a class="navbar-brand" href="#"><strong>ITSM</strong></a> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navnav"> <span class="navbar-toggler-icon"></span> </button> <div class="collapse navbar-collapse" id="navnav"> <div class="navbar-nav"> <a class="nav-item nav-link" id="go_home">Home</a> <a class="nav-item nav-link active" onclick="goToCreate(1);">New Issue</a> <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" id="reports" role="button" data-toggle="dropdown">Reports</a> <div class="dropdown-menu" id="reports_drop"> </div> </li> </div> <div class="col-5" id="searches_div"> </div> <div class="btn-group" role="group"> <button class="btn btn-sm btn-outline-secondary" type="button" onclick="processDisplayDropdown(true)"> Refresh </button> <button class="btn btn-sm btn-outline-secondary" type="button" onclick="document.prevpage.submit()"><span>«</span></button> <button class="btn btn-sm btn-outline-secondary" type="button" onclick="document.nextpage.submit()"><span>»</span></button> </div> <span class=" ml-1 badge badge-secondary" data-toggle="tooltip" data-placement="bottom" title="Tickets Loaded / Total" id="count_badge">$count</span> </div> <form class="form-inline my-2 mr-2"> <div class="input-group"> <input type="text" class="form-control" id="search_input" placeholder="Search..."> <div class="input-group-append"> <button class="btn btn-outline-secondary" type="button" onclick="goto_ticket()">Ticket</button> <button class="btn btn-outline-secondary" type="button" onclick="do_search()">Query</button> <button class="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" type="button" data-toggle="dropdown"></button> <div class="dropdown-menu" id="query_drop"> <a class="dropdown-item" onclick="update_search(this)">Title</a> <a class="dropdown-item" onclick="update_search(this)">Keyword</a> </div> </div> </div> </form> <button class="btn btn-outline-info" type="button" data-toggle="collapse" data-target="#hidden_opts"> 🛠 </button> </nav> </div>';
@@ -437,7 +438,7 @@ function create_navbar(){
     document.body.prepend(navbar);
 }
 
-function create_offset(){
+function create_offset() {
     let bar_height = parseInt(window.getComputedStyle(document.getElementById("justbar")).height);
 
     let offset = bar_height + (0.25 * bar_height);
@@ -445,7 +446,7 @@ function create_offset(){
     document.body.style.paddingTop = offset + "px";
 }
 
-function create_container(size, columns){
+function create_container(size, columns) {
     let fragment = document.createDocumentFragment();
 
     let container = document.createElement('div');
@@ -453,22 +454,22 @@ function create_container(size, columns){
     container.id = "cards";
 
     let spare = size % columns;
-    let rows = (size - spare) /  columns;
+    let rows = (size - spare) / columns;
 
-    for(let i = 0; i < rows; i++){
+    for (let i = 0; i < rows; i++) {
         let row = create_row();
 
-        for(let j = 0; j < columns; j++){
+        for (let j = 0; j < columns; j++) {
             row.appendChild(create_card());
         }
 
         container.appendChild(row);
     }
 
-    if(spare){
+    if (spare) {
         let row = create_row();
 
-        for(let i = 0; i < spare; i++){
+        for (let i = 0; i < spare; i++) {
             row.appendChild(create_card());
         }
 
@@ -482,34 +483,34 @@ function create_container(size, columns){
     return fragment;
 }
 
-function goto_ticket(){
-    goToDetails(document.getElementById("search_input").value , 1);
+function goto_ticket() {
+    goToDetails(document.getElementById("search_input").value, 1);
 }
 
-function do_search(){
-  let query = document.getElementById("search_input").value;
+function do_search() {
+    let query = document.getElementById("search_input").value;
 
-  if (query.match(/^\d{6,7}$/)) {
-    goToDetails(query, 1);
-  }else if (query === ""){
-    let uh_oh = confirm('You are about to ask FootPrints to return the ENTIRE ticket database. Proceed?');
-    if(uh_oh){
-      document.quickSearch.SEARCHS.value = query;
-      document.quickSearch.submit();
+    if (query.match(/^\d{6,7}$/)) {
+        goToDetails(query, 1);
+    } else if (query === "") {
+        let uh_oh = confirm('You are about to ask FootPrints to return the ENTIRE ticket database. Proceed?');
+        if (uh_oh) {
+            document.quickSearch.SEARCHS.value = query;
+            document.quickSearch.submit();
+        }
+    } else {
+        document.quickSearch.SEARCHS.value = query;
+        document.quickSearch.submit();
     }
-  }else{
-    document.quickSearch.SEARCHS.value = query;
-    document.quickSearch.submit();
-  }
 }
 
-function update_search(option){
+function update_search(option) {
     let searchby = option.textContent.toUpperCase();
     document.quickSearch.WSEARCH.value = searchby;
 
     const indicate = document.getElementById("query_drop");
 
-    if (searchby === "KEYWORD"){
+    if (searchby === "KEYWORD") {
         indicate.children[1].classList.add("active");
         indicate.children[0].classList.remove("active");
     } else {
