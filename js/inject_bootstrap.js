7dd6f3962b82189cfee528bf28a9b3ba523fc715
Mateusz Ryczek <mateusz@u.northwestern.edu>
2021-04-28 13:45:38 -0500

    JS works best when not declared as CSS

--- js/inject_bootstrap.js
+++ js/inject_bootstrap.js
@@ -16,11 +16,12 @@ function inject() {
     boot_css.crossorigin = "anonymous";
     document.head.appendChild(boot_css);
 
-    let boot_js = document.createElement("link");
+    let boot_js = document.createElement("script");
 
     boot_js.type = "application/javascript";
-    boot_js.rel = "stylesheet";
-    boot_js.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js";
+    boot_js.async = false;
+    boot_js.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js";
     boot_js.crossorigin = "anonymous";
+
     document.head.appendChild(boot_js);
 }
