1ee805025da89abdf6de3a07e5aa4a09be4a5b02
Mateusz Ryczek <mateusz@u.northwestern.edu>
2021-05-10 11:22:32 -0500

    Add error handling to ticket export

--- js/ticket_utils.js
+++ js/ticket_utils.js
@@ -1,23 +1,126 @@
+var prefix = 'table > tbody > tr > .x-grid3-td-hpColHeading_';
+
 function get_tickets_div() {
     return document.querySelectorAll(".x-grid3-row");
 }
 
-function ticket_to_json(ticket) {
-    let prefix = 'table > tbody > tr > .x-grid3-td-hpColHeading_';
+// Ticket number and title are always present on tickets
+function get_ticket_num(ticket) {
+    return ticket.querySelector(prefix + 'mr > div > span').innerText;
+}
 
-    let ticket_obj = {
-        number: ticket.querySelector(prefix + 'mr > div > span').innerText,
-        status: ticket.querySelector(prefix + 'status > div').innerText,
-        priority: ticket.querySelector(prefix + 'priority > div > span').innerText,
-        last_edit: ticket.querySelector(prefix + 'datetimeago > div').innerText,
-        closed_by: ticket.querySelector(prefix + 'closedby > div').innerText,
-        dept: ticket.querySelector(prefix + 'Departmental__bSupport > div').innerText,
-        title: ticket.querySelector(prefix + 'title > div > a').innerText,
-        desc: ticket.querySelector(prefix + 'title > div > a').nextSibling.data
-    };
+// Ticket number and title are always present on tickets
+function get_title(ticket) {
+    return ticket.querySelector(prefix + 'title > div > a').innerText;
+}
+
+function get_description(ticket) {
+    let desc = ticket.querySelector(prefix + 'title > div > a').nextSibling.data;
+
+    if (desc.length === 1) {
+        desc = 'Enable "Collapsed Description in Title" within your prefs to show previews!';
+    }
+
+    if (typeof desc === undefined) {
+        desc = 'Enable "Collapsed Description in Title" within your prefs to show previews!';
+    }
+
+    return desc;
+}
+
+function get_status(ticket) {
+    let status;
+
+    try {
+        status = ticket.querySelector(prefix + 'status > div').innerText;
+
+    } catch {
+        status = "DISABLED";
+    }
+    return status;
+}
+
+function get_last_edit(ticket) {
+    let last_edit;
+
+    try {
+        last_edit = ticket.querySelector(prefix + 'datetimeago > div').innerText;
+    } catch {
+        last_edit = 'DISABLED';
+    }
+
+    return last_edit;
+}
+
+function get_priority(ticket) {
+    let priority;
+
+    try {
+        priority = ticket.querySelector(prefix + 'priority > div > span').innerText;
+    } catch {
+        priority = "DISABLED";
+    }
+
+    return priority;
+}
+
+function get_dept_support(ticket) {
+    let support;
+
+    try {
+        support = ticket.querySelector(prefix + 'Departmental__bSupport > div').innerText;
+
+        if (support === '-') {
+            support = "none";
+        }
+    } catch {
+        support = "DISABLED";
+    }
+
+    return support;
+}
+
+function get_closed_by(ticket) {
+    let closer;
+
+    try {
+        closer = ticket.querySelector(prefix + 'closedby > div').innerText;
+    } catch {
+        closer = 'DISABLED';
+    }
+
+    return closer;
+}
+
+function ticket_to_json(ticket) {
+
+    let ticket_obj;
+
+    try {
+        ticket_obj = {
+            number: get_ticket_num(ticket),
+            status: get_status(ticket),
+            priority: get_priority(ticket),
+            last_edit: get_last_edit(ticket),
+            closed_by: get_closed_by(ticket),
+            dept: get_dept_support(ticket),
+            title: get_title(ticket),
+            desc: get_description(ticket)
+        };
+    } catch {
+        ticket_obj = {
+            number: ticket.querySelector(prefix + 'mr > div > span').innerText,
+            status: 'CATCH reached, contact(dev)',
+            priority: 'CATCH reached, contact(dev)',
+            last_edit: 'CATCH reached, contact(dev)',
+            closed_by: 'CATCH reached, contact(dev)',
+            dept: 'CATCH reached, contact(dev)',
+            title: ticket.querySelector(prefix + 'title > div > a').innerText,
+            desc: 'CATCH reached, contact(dev)'
+        };
+    }
 
     return ticket_obj;
-
 }
 
 function tickets_to_json() {
@@ -31,4 +134,6 @@ function tickets_to_json() {
     }
 
     console.log(JSON.stringify(ticket_array));
+
+    return ticket_array;
 }
